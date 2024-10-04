f6325bb8755d4f4f86114758df125afb0ceb6eed
FruziGame <138768045+FruziGame@users.noreply.github.com>
2024-10-04 13:48:19 +0000

    Deploying to gh-pages from @ FruziGame/goit-js-hw-11@ad39a28136ea3d089cad847031f0351c35cdcf6a 🚀

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-import{S as u,i as d}from"./assets/vendor-5ObWk2rO.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))i(t);new MutationObserver(t=>{for(const e of t)if(e.type==="childList")for(const a of e.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&i(a)}).observe(document,{childList:!0,subtree:!0});function s(t){const e={};return t.integrity&&(e.integrity=t.integrity),t.referrerPolicy&&(e.referrerPolicy=t.referrerPolicy),t.crossOrigin==="use-credentials"?e.credentials="include":t.crossOrigin==="anonymous"?e.credentials="omit":e.credentials="same-origin",e}function i(t){if(t.ep)return;t.ep=!0;const e=s(t);fetch(t.href,e)}})();const m=document.querySelector(".gallery"),p=document.querySelector(".loader"),f=new u(".gallery a",{captionsData:"alt"}),g={render:h};function h(r){const o=r.hits.map(s=>`<div>
-        <a href="${s.largeImageURL}" class="image-link">
-                <img src="${s.webformatURL}" class="image" alt="${s.tags}" title="${s.tags}" />
+import{S as m,i as n}from"./assets/vendor-5ObWk2rO.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))o(t);new MutationObserver(t=>{for(const r of t)if(r.type==="childList")for(const i of r.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&o(i)}).observe(document,{childList:!0,subtree:!0});function e(t){const r={};return t.integrity&&(r.integrity=t.integrity),t.referrerPolicy&&(r.referrerPolicy=t.referrerPolicy),t.crossOrigin==="use-credentials"?r.credentials="include":t.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function o(t){if(t.ep)return;t.ep=!0;const r=e(t);fetch(t.href,r)}})();const h=document.querySelector(".gallery"),p=document.querySelector(".loader"),f=new m(".gallery a",{captionsData:"alt"}),g={render:y};function y(a){const s=a.hits.map(e=>`<div>
+        <a href="${e.largeImageURL}" class="image-link">
+                <img src="${e.webformatURL}" class="image" alt="${e.tags}" title="${e.tags}" />
                 <div class="data">
                     <div>
                         <h2 class="data-image">Likes</h2>
-                        <span class="data-quantity">${s.likes}</span>
+                        <span class="data-quantity">${e.likes}</span>
                     </div>
                     <div>
                         <h2 class="data-image">Views</h2>
-                        <span class="data-quantity">${s.views}</span>
+                        <span class="data-quantity">${e.views}</span>
                     </div>
                     <div>
                         <h2 class="data-image">Comments</h2>
-                        <span class="data-quantity">${s.comments}</span>
+                        <span class="data-quantity">${e.comments}</span>
                     </div>
                     <div>
                         <h2 class="data-image">Downloads</h2>
-                        <span class="data-quantity">${s.downloads}</span>
+                        <span class="data-quantity">${e.downloads}</span>
                     </div>
                 </div>
         </a></div>
-    `).join("");m.innerHTML=o,p.classList.remove("visible"),f.refresh()}const n=document.querySelector(".loader");function y(r){n.classList.add("visible");const o="https://pixabay.com/api/",s="46290930-3746f5b7b54e4eb8c1145cc43",i=`&q=${encodeURIComponent(r)}&image_type=photo&orientation=horizontal&safesearch=true`,t=`${o}?key=${s}${i}`;fetch(t).then(e=>e.json()).then(e=>{console.log(e),e.hits.length===0?(d.show({color:"red",message:"Sorry, there are no images matching your search query. Please try again!",position:"topRight"}),n.classList.remove("visible")):(console.log(e),g.render(e))}).catch(e=>{console.error("Ошибка:",e)})}const v={getImages:y},c=document.querySelector(".form"),l=document.querySelector(".form-input");c.addEventListener("submit",r=>{if(r.preventDefault(),l.value.trim()==="")d.show({color:"white",message:"Please fill the input",position:"topRight"});else{const o=l.value;console.log(o),v.getImages(o)}c.reset()});
+    `).join("");h.innerHTML=s,p.classList.remove("visible"),f.refresh()}const c=document.querySelector(".loader"),v="https://pixabay.com/api/",L="46290930-3746f5b7b54e4eb8c1145cc43";function u(a){const s=`&q=${encodeURIComponent(a)}&image_type=photo&orientation=horizontal&safesearch=true`,e=`${v}?key=${L}${s}`;return fetch(e).then(o=>o.json()).then(o=>o).catch(o=>{throw console.error("Ошибка:",o),o})}function b(a){const s=document.querySelector(".gallery");c.classList.add("visible"),u(a).then(e=>{s.innerHTML="",e.hits.length===0?n.show({color:"red",message:"Sorry, there are no images matching your search query. Please try again!",position:"topRight"}):g.render(e)}).catch(e=>{n.show({color:"red",message:"An error occurred during the request.",position:"topRight"})}).finally(()=>{c.classList.remove("visible")})}const q={getImages:u,handleImages:b},l=document.querySelector(".form"),d=document.querySelector(".form-input");l.addEventListener("submit",a=>{if(a.preventDefault(),d.value.trim()==="")n.show({color:"white",message:"Please fill the input",position:"topRight"});else{const s=d.value;q.handleImages(s)}l.reset()});
 //# sourceMappingURL=index.js.map
